d948c120fe322c425f1e2126919b0da666454355
BuhoyCoder <61500605+BuhoyCoder@users.noreply.github.com>
2020-05-27 16:41:02 +0300

    Create Callback.php

---
+++ src/Callback.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace BuhoyCoder\VK;
+
+class Callback
+{
+	private $ctx;
+	
+	private $data = [];
+	
+	private $debug_mode;
+	
+	public function __construct(VkApi $vk, string $confirm_str = '', bool $debug_mode = false)
+	{
+		$this->debug_mode = $debug_mode;
+		$this->data = json_decode(file_get_contents('php://input'), true);
+
+		if ($this->data['type'] === 'confirmation') {
+			exit($confirm_str);
+		}
+		
+		$this->closeRequest();
+		
+		$this->ctx = new Context($vk, $this->data);
+	}
+	
+	private function closeRequest()
+	{
+		set_time_limit(0);
+
+		if ($this->debug_mode) {
+			error_reporting(E_ALL);
+			ini_set('display_errors', 'on');
+			ini_set('display_startup_errors', 'on');
+			echo 'ok';
+		} else {
+			ini_set('display_errors', 'off');
+
+			// для Nginx
+			if (is_callable('fastcgi_finish_request')) {
+				echo 'ok';
+				session_write_close();
+				fastcgi_finish_request();
+			} else {
+				// для Apache
+				ignore_user_abort(true);
+
+				ob_start();
+				header('Content-Encoding: none');
+				header('Content-Length: 2');
+				header('Connection: close');
+				echo 'ok';
+				ob_end_flush();
+				flush();
+			}
+		}
+	}
+	
+	public function on(string $name, callable $handler)
+	{
+		if ($this->data['type'] === $name) {
+			$handler($this->ctx);
+		}
+	}
+}
